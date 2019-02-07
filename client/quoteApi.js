7722b323b271909a7caf8ca620bfb0497f4de6c9
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-02-08 12:46:56 +1300

    Can now see lucky quote

--- client/quoteApi.js
+++ client/quoteApi.js
@@ -4,7 +4,9 @@ const url = 'http://localhost:3000/api/v1'
 
 export function getQuote () {
   return request.get(`${url}/quote`)
-    .then(res => res)
+    .then(res => {
+      return res.body
+    })
     .catch(err => {
       if (err) throw Error('You don\'t need a quote')
     })
