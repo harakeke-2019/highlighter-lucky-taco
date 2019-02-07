5c5a949af4d463ad4f9e98fb9de91c810bdd8f6d
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-02-08 11:43:18 +1300

    pulled from master

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Home extends React.Component {
   constructor (props) {
@@ -13,7 +14,7 @@ export default class Home extends React.Component {
       <div>
         <button href='/aries'><h1>Aries</h1><br/>March 21 - April 19</button>
         <button href='/taurus'><h1>Taurus</h1><br/>April 20 - May 20</button>
-        <button href='gemini'><h1>Gemini</h1><br/>May 21 - June 20</button>
+        <button href='/gemini'><h1>Gemini</h1><br/>May 21 - June 20</button>
         <button href='cancer'><h1>Cancer</h1><br/>June 21 - July 22</button>
         <button href='leo'><h1>Leo</h1><br/>July 23 - August 22</button>
         <button href='virgo'><h1>Virgo</h1><br/>August 23 - September 22</button>
