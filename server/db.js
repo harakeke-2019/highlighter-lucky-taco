bb21615247002bcaac1ffbf8af6437792ea43e87
Lane Wirihana le Prevost-Smith <lanelps@gmail.com>
2019-02-08 11:36:10 +1300

    Can get the tacos

--- server/db.js
+++ server/db.js
@@ -1,3 +1,25 @@
 // set up database stuff
 // getTacos
 // getStarSigns
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+  getTacos
+}
+
+function getTacos (db = connection) {
+  const arr = getFourRandomNumbers()
+  console.log(arr)
+  return db('taco-fillings').where('id', arr[0]).orWhere('id', arr[1]).orWhere('id', arr[2]).orWhere('id', arr[3])
+}
+
+function getFourRandomNumbers () {
+  let arr = []
+  while (arr.length < 4) {
+    const ran = Math.floor(Math.random() * 8 + 1)
+    if (!arr.find(elem => elem === ran)) arr.push(ran)
+  }
+  return arr
+}
