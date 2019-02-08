f7254dfc97958c378130fe99811baad5ae7a5569
Francinebc <francinebc.mail@gmail.com>
2019-02-08 13:58:21 +1300

    Refactored star signs to use API

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {getStarSigns} from '../starSignApi'
 
 export default class Home extends React.Component {
   constructor (props) {
@@ -9,25 +10,24 @@ export default class Home extends React.Component {
     }
   }
 
+  componentDidMount () {
+    getStarSigns()
+      .then(starSigns => {
+        console.log(starSigns)
+        this.setState({ starSigns: starSigns })
+      })
+      .catch(err => {
+        console.error('Error:', err)
+      })
+  }
+
   render () {
     return (
       <div>
-        <div className='button'><Link to ='/taurus'><h1>Taurus</h1><br/>May 21 - June 20</Link></div>
-        <div className='button'><Link to ='/gemini'><h1>Gemini</h1><br/>May 21 - June 20</Link></div>
-        <div className='button'><Link to ='/cancer'><h1>Cancer</h1><br/>June 21 - July 22</Link></div>
-        <div className='button'><Link to ='/leo'><h1>Leo</h1><br/>July 23 - August 22</Link></div>
-        <div className='button'><Link to ='/aries'><h1>Aries</h1><br/>March 21 - April 19</Link></div>
-        <div className='button'><Link to ='/virgo'><h1>Virgo</h1><br/>August 23 - September 22</Link></div>
-        <div className='button'><Link to ='/libra'><h1>Libra</h1><br/>September 23 - October 22</Link></div>
-        <div className='button'><Link to ='/scorpio'><h1>Scorpio</h1><br/>October 23 - November 21</Link></div>
-        <div className='button'><Link to ='/sagitarius'><h1>Sagitarius</h1><br/>November 22 - December 21</Link></div>
-        <div className='button'><Link to ='/capricorn'><h1>Capricorn</h1><br/>December 22 - January 19</Link></div>
-        <div className='button'><Link to ='/aqurius'><h1>Aquarius</h1><br/>January 20 - February 18</Link></div>
-        <div className='button'><Link to ='/pisces'><h1>Pisces</h1><br/>February 19 - March 20</Link></div>
-        {/* {this.state.starSigns.map(sign => {
-          return <h1 key={sign.name}> {sign.name} </h1>
+        {this.state.starSigns.length > 0 && this.state.starSigns.map(sign => {
+          return <div className='button' key={sign.name}><Link to ={`/${sign.name}`}><h1>{sign.name}</h1><br/>{sign.date}</Link></div>
         })
-        } */}
+        }
       </div>
     )
   }
